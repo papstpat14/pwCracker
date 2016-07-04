2f105fe82f4405f74681808207ffa82d9e456b43
Stefano1993 <stefan.obendrauf@hotmail.de>
2016-07-04 19:12:50 +0200

    removed old server with express and added new one without express

---
+++ server/message_handler/messageHandler.js
@@ -0,0 +1,32 @@
+// Initiate Websocket Server
+function initSocketServer(wsServer){
+	// wsServer is not set -> return
+	if(!wsServer){
+		return;
+	}
+	
+	wsServer.on('request', function (request) {
+
+		var connection = request.accept('chat-protocol', request.origin);
+		console.log((new Date()) + ' Connection accepted.');
+
+		connection.on('message', function (message) {
+			console.log('Received Message: ' + message.utf8Data);
+
+			var respJSON = JSON.parse(message.utf8Data);
+			
+			// TODO MessageHandler Team -> implementation for Handling messages
+			respJSON.value = "-- zu ermitteln --";
+			connection.sendUTF(JSON.stringify(respJSON));
+		});
+
+		connection.on('close', function (connection) {
+			console.log("Connection close: " + connection);
+		});
+	});
+}
+
+// Export Module with param wsServer
+module.exports = (wsServer) => {
+	initSocketServer(wsServer);
+}
---
+++ server/server.js
@@ -0,0 +1,64 @@
+//Version: 1.0.0
+//LasrChanged: 04.07.2016
+//Authors: Michael Koch, Obendrauf Stefan
+
+var http = require("http");
+var path = require("path");
+var url = require("url");
+var fs = require("fs");
+var socketServer = require('websocket').server;
+var messageHandler = require("./message_handler/messageHandler.js");
+
+// Get port from package.json or default 8080
+var port = process.env.npm_package_config_port || 8080;
+
+var server = http.createServer(function(req,res){
+	// Parse Request
+    var requestPath = url.parse(req.url).pathname;
+	
+	// Check Request Path
+	if(requestPath === "/"){
+		requestPath = "../frontend/index.html"
+	} else if (requestPath.indexOf("/js/") > -1 || requestPath.indexOf("/css/") > -1){
+	    requestPath = "../frontend/" + requestPath;
+	}
+	
+	// Join with current working directory
+	var fullPath = path.join(process.cwd(),requestPath);
+	
+	// Look for file
+    fs.exists(fullPath,function(exists){
+		// Not found -> 404
+        if(!exists){
+            res.writeHeader(404, {"Content-Type": "text/plain"});  
+            res.write("404 Not Found\n");  
+            res.end();
+        }
+		// File found
+        else{
+            fs.readFile(fullPath, "binary", function(err, file) {  
+                 if(err) {  
+                     res.writeHeader(500, {"Content-Type": "text/plain"});  
+                     res.write(err + "\n");  
+                     res.end();  
+                
+                 }  
+                 else{
+                    res.writeHeader(200);  
+                    res.write(file, "binary");  
+                    res.end();
+                }
+            });
+        }
+    });
+}).listen(port);
+
+console.log("Server Running on port: " + port);        
+
+// Create SocketServer
+var wsServer = new socketServer({
+    httpServer: server,
+    autoAcceptConnections: false
+});
+
+messageHandler(wsServer);
